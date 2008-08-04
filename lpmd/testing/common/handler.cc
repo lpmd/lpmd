28b9f020da22459a655471f8c4b2565c9850e2d9
sdavis <sdavis@65fed1b5-ef09-4d86-bbe6-19692ee45b87>
2008-08-04 20:40:18 +0000

    Termina correctamente (no muestra error de "cell") al llamar sin argumentos.

--- lpmd/testing/common/handler.cc
+++ lpmd/testing/common/handler.cc
@@ -193,6 +193,7 @@ void CommonHandler::SetOptionVariables(CommonCmdLineParser & clp, ParamList & ov
 void CommonHandler::Execute(CommonCmdLineParser & clp)
 {
  std::list<std::string> args = clp.Arguments();
+ if (args.size() == 1) ShowHelp();
  args.pop_front();
  if (args.size() == 1) { clp.AssignParameter("inputfile-file", args.front()); }
  SetVerbose(false);
